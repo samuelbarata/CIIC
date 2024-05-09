6f627ff871042f4c45f3deb875510731a722d980
samuelbarata <samuel@barata.pt>
2024-05-09 11:16:16 +0100

    qnd pensares, diz-me alguma coisa

--- Proj1/main.py
+++ Proj1/main.py
@@ -6,7 +6,7 @@ DATASET_PATH = 'CI4IoT23-24_Proj1_SampleData.csv'
 
 # TODO: Remove this line https://github.com/aresio/simpful/tree/master/examples
 
-# Create a fuzzy system
+# CPU Memory
 FS = FuzzySystem()
 
 # Processor Load
@@ -22,17 +22,65 @@ MU_3 = FuzzySet(function=Triangular_MF(a=0.70, b=1.0, c=1.0), term="high")
 FS.add_linguistic_variable("MemoryUsage", LinguisticVariable([MU_1, MU_2, MU_3], concept="Memory Usage", universe_of_discourse=[0,1]))
 
 # CpuMem
-T_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
-T_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.75, c=0.9), term="medium")
+T_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.60), term="low")
+T_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.9), term="medium")
 T_3 = FuzzySet(function=Triangular_MF(a=0.80, b=1.0, c=1.0), term="high")
 FS.add_linguistic_variable("CpuMem", LinguisticVariable([T_1, T_2, T_3], universe_of_discourse=[0,1]))
 
 
 # Define fuzzy rules
-R1 = "IF (ProcessorLoad IS high) OR (MemoryUsage IS high) THEN (CpuMem IS high)"
-R2 = "IF (ProcessorLoad IS medium) OR (MemoryUsage IS medium) THEN (CpuMem IS medium)"
-R3 = "IF (ProcessorLoad IS low) OR (MemoryUsage IS low) THEN (CpuMem IS low)"
-FS.add_rules([R1, R2, R3])
+R1_1 = "IF (ProcessorLoad IS high) OR (MemoryUsage IS high) THEN (CpuMem IS high)"
+R1_2 = "IF (ProcessorLoad IS medium) OR (MemoryUsage IS medium) THEN (CpuMem IS medium)"
+R1_3 = "IF (ProcessorLoad IS low) OR (MemoryUsage IS low) THEN (CpuMem IS low)"
+FS.add_rules([R1_1, R1_2, R1_3])
+
+# Network
+FS2 = FuzzySystem()
+
+# InpNetThroughput
+IN_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
+IN_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.8), term="medium")
+IN_3 = FuzzySet(function=Triangular_MF(a=0.60, b=1.0, c=1.0), term="high")
+FS2.add_linguistic_variable("InpNetThroughput", LinguisticVariable([IN_1, IN_2, IN_3], concept="Input Network Throughput", universe_of_discourse=[0,1]))
+
+# OutNetThroughput
+OUT_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
+OUT_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.8), term="medium")
+OUT_3 = FuzzySet(function=Triangular_MF(a=0.60, b=1.0, c=1.0), term="high")
+FS2.add_linguistic_variable("OutNetThroughput", LinguisticVariable([OUT_1, OUT_2, OUT_3], concept="Output Network Throughput", universe_of_discourse=[0,1]))
+
+# OutBandwidth
+OB_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
+OB_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.8), term="medium")
+OB_3 = FuzzySet(function=Triangular_MF(a=0.60, b=1.0, c=1.0), term="high")
+FS2.add_linguistic_variable("OutBandwidth", LinguisticVariable([OB_1, OB_2, OB_3], concept="Output Bandwidth", universe_of_discourse=[0,1]))
+
+# Latency
+L_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
+L_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.8), term="medium")
+L_3 = FuzzySet(function=Triangular_MF(a=0.60, b=1.0, c=1.0), term="high")
+FS2.add_linguistic_variable("Latency", LinguisticVariable([L_1, L_2, L_3], concept="Latency", universe_of_discourse=[0,1]))
+
+# Network Usage
+NU_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.70), term="low")
+NU_2 = FuzzySet(function=Triangular_MF(a=0.5, b=0.70, c=0.8), term="medium")
+NU_3 = FuzzySet(function=Triangular_MF(a=0.60, b=1.0, c=1.0), term="high")
+FS2.add_linguistic_variable("NetUsage", LinguisticVariable([NU_1, NU_2, NU_3], concept="Network Usage", universe_of_discourse=[0,1]))
+
+# Define Rules
+R2_1 = "IF (InpNetThroughput IS high) OR (OutNetThroughput IS high) OR (OutBandwidth IS high) OR (Latency IS high) THEN (NetUsage IS high)"
+
+
+
+
+FS_OUT = FuzzySystem()
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     df = pd.read_csv(DATASET_PATH)
@@ -54,11 +102,23 @@ if __name__ == '__main__':
         latency_variation = row['V_Latency']
         clp_variation = row['CLPVariation']
 
-        # Set antecedents values
+        # Set values
         FS.set_variable("ProcessorLoad", process_load)
         FS.set_variable("MemoryUsage", memory_usage)
+
+        FS2.set_variable("InpNetThroughput", input_throughput)
+        FS2.set_variable("OutNetThroughput", output_throughput)
+        FS2.set_variable("OutBandwidth", output_available_bandwidth)
+        FS2.set_variable("Latency", latency)
+
         # Evaluate the system
         cpu_mem = FS.Mamdani_inference(["CpuMem"])
+        network = FS2.Mamdani_inference(["NetUsage"])
+
+
+
+
 
 
         print(f"CPU: {process_load}; Mem: {memory_usage}; {cpu_mem['CpuMem']}")
+        break
