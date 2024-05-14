9c704df0301b82866efc343eecaff3f9c1d38029
sandr-castilho <sandr-castilho@users.noreply.github.com>
2024-05-14 15:51:25 +0100

    add diagram, update FS variables names

--- Proj1/main.py
+++ Proj1/main.py
@@ -6,26 +6,26 @@ import logging
 # TODO: Remove this line https://github.com/aresio/simpful/tree/master/examples
 
 # #### HARDWARE RESOURCES ####
-FS = FuzzySystem(show_banner=False)
+FS_HDR = FuzzySystem(show_banner=False)
 
 # Processor Load
 PL_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.45, d=0.55), term="low")
 PL_2 = FuzzySet(function=Trapezoidal_MF(a=0.35, b=0.55, c=0.65, d=0.85), term="medium")
 PL_3 = FuzzySet(function=Trapezoidal_MF(a=0.55, b=0.75, c=1.0, d=1.0), term="high")
-FS.add_linguistic_variable("ProcessorLoad", LinguisticVariable([PL_1, PL_2, PL_3], concept="Processor Load", universe_of_discourse=[0,1]))
+FS_HDR.add_linguistic_variable("ProcessorLoad", LinguisticVariable([PL_1, PL_2, PL_3], concept="Processor Load", universe_of_discourse=[0,1]))
 
 # Memory Usage
 MU_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.45, d=0.55), term="low")
 MU_2 = FuzzySet(function=Trapezoidal_MF(a=0.35, b=0.55, c=0.65, d=0.85), term="medium")
 MU_3 = FuzzySet(function=Trapezoidal_MF(a=0.55, b=0.75, c=1.0, d=1.0), term="high")
-FS.add_linguistic_variable("MemoryUsage", LinguisticVariable([MU_1, MU_2, MU_3], concept="Memory Usage", universe_of_discourse=[0,1]))
+FS_HDR.add_linguistic_variable("MemoryUsage", LinguisticVariable([MU_1, MU_2, MU_3], concept="Memory Usage", universe_of_discourse=[0,1]))
 
 # Hardware Resources
 T_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.4, d=0.5), term="low")
 T_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.7, d=0.8), term="medium")
 T_3 = FuzzySet(function=Trapezoidal_MF(a=0.7, b=0.8, c=1.0, d=1.0), term="high")
 T_4 = FuzzySet(function=Trapezoidal_MF(a=0.8, b=0.9, c=1.0, d=1.0), term="very_high")
-FS.add_linguistic_variable("CpuMem", LinguisticVariable([T_1, T_2, T_3, T_4], concept="Hardware Resources", universe_of_discourse=[0,1]))
+FS_HDR.add_linguistic_variable("CpuMem", LinguisticVariable([T_1, T_2, T_3, T_4], concept="Hardware Resources", universe_of_discourse=[0,1]))
 
 """
    Memory Usage
@@ -46,32 +46,32 @@ R1.append("IF (ProcessorLoad IS medium) AND (MemoryUsage IS medium) THEN (CpuMem
 R1.append("IF (ProcessorLoad IS low) AND (MemoryUsage IS medium) THEN (CpuMem IS medium)")
 R1.append("IF (ProcessorLoad IS medium) AND (MemoryUsage IS low) THEN (CpuMem IS medium)")
 R1.append("IF (ProcessorLoad IS low) AND (MemoryUsage IS low) THEN (CpuMem IS low)")
-FS.add_rules(R1)
+FS_HDR.add_rules(R1)
 
 
 # #### NETWORK USAGE ####
 
 # ### Output Congestion ###
 
-FS2 = FuzzySystem(show_banner=False)
+FS_OUT = FuzzySystem(show_banner=False)
 
 # Output Net Throughput
 OUT_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.35, d=0.45), term="low")
 OUT_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.55, d=0.75), term="medium")
 OUT_3 = FuzzySet(function=Trapezoidal_MF(a=0.6, b=0.75, c=1.0, d=1.0), term="high")
-FS2.add_linguistic_variable("OutNetThroughput", LinguisticVariable([OUT_1, OUT_2, OUT_3], concept="Output Network Throughput", universe_of_discourse=[0,1]))
+FS_OUT.add_linguistic_variable("OutNetThroughput", LinguisticVariable([OUT_1, OUT_2, OUT_3], concept="Output Network Throughput", universe_of_discourse=[0,1]))
 
 # Available Output Bandwidth
 OB_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.35, d=0.45), term="low")
 OB_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.55, d=0.75), term="medium")
 OB_3 = FuzzySet(function=Trapezoidal_MF(a=0.6, b=0.75, c=1.0, d=1.0), term="high")
-FS2.add_linguistic_variable("AvailOutBandwidth", LinguisticVariable([OB_1, OB_2, OB_3], concept="Available Output Bandwidth", universe_of_discourse=[0,1]))
+FS_OUT.add_linguistic_variable("AvailOutBandwidth", LinguisticVariable([OB_1, OB_2, OB_3], concept="Available Output Bandwidth", universe_of_discourse=[0,1]))
 
 # Output Congestion
 OC_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.35, d=0.45), term="low")
 OC_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.55, d=0.75), term="medium")
 OC_3 = FuzzySet(function=Trapezoidal_MF(a=0.6, b=0.75, c=1.0, d=1.0), term="high")
-FS2.add_linguistic_variable("OutCongestion", LinguisticVariable([OC_1, OC_2, OC_3], concept="Output Congestion", universe_of_discourse=[0,1]))
+FS_OUT.add_linguistic_variable("OutCongestion", LinguisticVariable([OC_1, OC_2, OC_3], concept="Output Congestion", universe_of_discourse=[0,1]))
 
 """
   Output Bandwith Available
@@ -97,25 +97,25 @@ R2.append("IF (OutNetThroughput IS high) AND (AvailOutBandwidth IS medium) THEN 
 R2.append("IF (OutNetThroughput IS low) AND (AvailOutBandwidth IS high) THEN (OutCongestion IS low)")
 R2.append("IF (OutNetThroughput IS medium) AND (AvailOutBandwidth IS high) THEN (OutCongestion IS medium)")
 R2.append("IF (OutNetThroughput IS high) AND (AvailOutBandwidth IS high) THEN (OutCongestion IS high)")
-FS2.add_rules(R2)
+FS_OUT.add_rules(R2)
 
 
 # #### COMPUTING LOAD PERCENTAGE (CLP) ####
 
 FS_CLP = FuzzySystem(show_banner=False)
 
-L_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.2, d=0.4), term="low")
-L_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.6, d=0.8), term="medium")
-L_3 = FuzzySet(function=Trapezoidal_MF(a=0.6, b=0.8, c=1.0, d=1.0), term="high")
-FS_CLP.add_linguistic_variable("Latency", LinguisticVariable([L_1, L_2, L_3], concept="Latency", universe_of_discourse=[0,1]))
+# L_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.2, d=0.4), term="low")
+# L_2 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=0.6, d=0.8), term="medium")
+# L_3 = FuzzySet(function=Trapezoidal_MF(a=0.6, b=0.8, c=1.0, d=1.0), term="high")
+# FS_CLP.add_linguistic_variable("Latency", LinguisticVariable([L_1, L_2, L_3], concept="Latency", universe_of_discourse=[0,1]))
 
 FS_CLP.add_linguistic_variable("CpuMem", LinguisticVariable([T_1, T_2, T_3, T_4], concept="Hardware Resources", universe_of_discourse=[0,1]))
 FS_CLP.add_linguistic_variable("OutCongestion", LinguisticVariable([OC_1, OC_2, OC_3], concept="Output Congestion", universe_of_discourse=[0,1]))
 
 
-IN_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.5, d=0.7), term="low")
-IN_3 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=1.0, d=1.0), term="high")
-FS_CLP.add_linguistic_variable("InpNetThroughput", LinguisticVariable([IN_1, IN_3], concept="Input Network Throughput", universe_of_discourse=[0,1]))
+# IN_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=0.5, d=0.7), term="low")
+# IN_3 = FuzzySet(function=Trapezoidal_MF(a=0.3, b=0.5, c=1.0, d=1.0), term="high")
+# FS_CLP.add_linguistic_variable("InpNetThroughput", LinguisticVariable([IN_1, IN_3], concept="Input Network Throughput", universe_of_discourse=[0,1]))
 
 
 CLP_1 = FuzzySet(function=Trapezoidal_MF(a=-1, b=-1, c=-0.6, d=-0.25), term="decrease_much")
@@ -160,13 +160,13 @@ def read_input(FILE_NAME):
    return df
 
 def find_clp(memory_usage, process_load, input_throughput, output_throughput, output_available_bandwidth, latency, memory_usage_variation, process_load_variation, input_throughput_variation, output_throughput_variation, output_available_bandwidth_variation, latency_variation, clp_variation=None):
-    FS.set_variable("ProcessorLoad", process_load)
-    FS.set_variable("MemoryUsage", memory_usage)
-    cpu_mem = FS.Mamdani_inference()['CpuMem']
+    FS_HDR.set_variable("ProcessorLoad", process_load)
+    FS_HDR.set_variable("MemoryUsage", memory_usage)
+    cpu_mem = FS_HDR.Mamdani_inference()['CpuMem']
 
-    FS2.set_variable("OutNetThroughput", output_throughput)
-    FS2.set_variable("AvailOutBandwidth", output_available_bandwidth)
-    out_congestion = FS2.Mamdani_inference()["OutCongestion"]
+    FS_OUT.set_variable("OutNetThroughput", output_throughput)
+    FS_OUT.set_variable("AvailOutBandwidth", output_available_bandwidth)
+    out_congestion = FS_OUT.Mamdani_inference()["OutCongestion"]
 
     FS_CLP.set_variable("Latency", latency)
     FS_CLP.set_variable("InpNetThroughput", input_throughput)
@@ -188,24 +188,24 @@ if __name__ == '__main__':
     SHOW_3D = True
     SHOW_2D = False
     LOGGING_LEVEL = logging.INFO
-    LOGGING_LEVEL = logging.DEBUG
+    #LOGGING_LEVEL = logging.DEBUG
     DATASET_PATH = 'CI4IoT23-24_Proj1_SampleData.csv'
 
     df = read_input(DATASET_PATH)
     logging.basicConfig(level=LOGGING_LEVEL, format='%(levelname)s: %(message)s')
 
     if SHOW_3D:
-        # fig = FS.plot_surface(variables=['ProcessorLoad','MemoryUsage'], output='CpuMem')
-        # fig2 = FS2.plot_surface(variables=['OutNetThroughput','AvailOutBandwidth'], output='OutCongestion')
+        # fig = FS_HDR.plot_surface(variables=['ProcessorLoad','MemoryUsage'], output='CpuMem')
+        # fig2 = FS_OUT.plot_surface(variables=['OutNetThroughput','AvailOutBandwidth'], output='OutCongestion')
         fig4 = FS_CLP.plot_surface(variables=['CpuMem','OutCongestion'], output='CLP')
 
     if SHOW_2D:
-        FS.plot_variable("ProcessorLoad")
-        FS.plot_variable("MemoryUsage")
-        FS.plot_variable("CpuMem")
-        FS2.plot_variable("OutNetThroughput")
-        FS2.plot_variable("AvailOutBandwidth")
-        FS2.plot_variable("OutCongestion")
+        FS_HDR.plot_variable("ProcessorLoad")
+        FS_HDR.plot_variable("MemoryUsage")
+        FS_HDR.plot_variable("CpuMem")
+        FS_OUT.plot_variable("OutNetThroughput")
+        FS_OUT.plot_variable("AvailOutBandwidth")
+        FS_OUT.plot_variable("OutCongestion")
         FS_CLP.plot_variable("CpuMem")
         FS_CLP.plot_variable("OutCongestion")
         FS_CLP.plot_variable("CLP")
