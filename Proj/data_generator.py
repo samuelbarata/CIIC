4241ce2e15c9333d90281eb1ccbe0a36f0bbf1ca
sandr-castilho <sandr-castilho@users.noreply.github.com>
2024-05-20 15:21:05 +0100

    update data_generator

--- Proj/data_generator.py
+++ Proj/data_generator.py
@@ -1,10 +1,22 @@
 import csv
 import random
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import main
 
 headers = 'MemoryUsage,ProcessorLoad,InpNetThroughput,OutNetThroughput,OutBandwidth,Latency,V_MemoryUsage,V_ProcessorLoad,V_InpNetThroughput,V_OutNetThroughput,V_OutBandwidth,V_Latency'.split(',')
 
+LEARN_DATA_INPUT = 'data/data_10_rng.csv'
+LEARN_DATA_OUTPUT = 'data/TestResult-FSS_10_rng.csv'
+TEST_DATA_INPUT = 'data/data_rng'
+TEST_DATA_OUTPUT = 'data/TestResult-FSS_rng.csv'
 
-with open('data/data_10_rng.csv', 'w', newline='') as file:
+# ## TRAIN DATA ##
+
+print("Generating train inputs")
+## generate inputs
+with open(LEARN_DATA_INPUT, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(headers)
     MIN = 0
@@ -17,9 +29,22 @@ with open('data/data_10_rng.csv', 'w', newline='') as file:
             for OutNetThroughput in RANGE:
                 for OutBandwidth in RANGE:
                     writer.writerow([MemUsage/100, ProcessorLoad/100, random.randint(0, 100)/100, OutNetThroughput/100, OutBandwidth/100, random.randint(0, 100)/100, random.randint(0, 100)/100, random.randint(0, 100)/100, random.randint(0, 100)/100, random.randint(0, 100)/100, random.randint(0, 100)/100, random.randint(0, 100)/100])
+print("Train inputs generated")
 
+print("Generating train outputs (fuzzy system)")
+## get outputs from fuzzy system
+with open(LEARN_DATA_OUTPUT, 'w') as f:
+    f.write("MemoryUsage,ProcessorLoad,InpNetThroughput,OutNetThroughput,OutBandwidth,Latency,V_MemoryUsage,V_ProcessorLoad,V_InpNetThroughput,V_OutNetThroughput,V_OutBandwidth,V_Latency,CLPVariation")
+    for index, row in main.read_input(LEARN_DATA_INPUT).iterrows():
+        clp = main.find_clp(row['MemoryUsage'], row['ProcessorLoad'], row['InpNetThroughput'], row['OutNetThroughput'], row['OutBandwidth'], row['Latency'], row['V_MemoryUsage'], row['V_ProcessorLoad'], row['V_InpNetThroughput'], row['V_OutNetThroughput'], row['V_OutBandwidth'], row['V_Latency'])
+        f.write(f"\n{row['MemoryUsage']},{row['ProcessorLoad']},{row['InpNetThroughput']},{row['OutNetThroughput']},{row['OutBandwidth']},{row['Latency']},{row['V_MemoryUsage']},{row['V_ProcessorLoad']},{row['V_InpNetThroughput']},{row['V_OutNetThroughput']},{row['V_OutBandwidth']},{row['V_Latency']},{clp}")
+print("Train outputs (fuzzy system) generated")
 
-with open('data_rng_v2.csv', 'w', newline='') as file:
+# ## TEST (and Validation) DATA ##
+
+print("Generating train inputs")
+## generate inputs
+with open(TEST_DATA_INPUT, 'w', newline='') as file:
     writer = csv.writer(file) 
     writer.writerow(headers)
     for _ in range(10000000):
@@ -36,4 +61,27 @@ with open('data_rng_v2.csv', 'w', newline='') as file:
         V_OutBandwidth = random.randint(0, 100)
         V_Latency = random.randint(0, 100)
         writer.writerow([MemUsage/100, ProcessorLoad/100, InpNetThroughput/100, OutNetThroughput/100, OutBandwidth/100, Latency/100, V_MemoryUsage/100, V_ProcessorLoad/100, V_InpNetThroughput/100, V_OutNetThroughput/100, V_OutBandwidth/100, V_Latency/100])
+print("Train inputs generated")
 
+print("Generating train outputs (fuzzy system)")
+## get outputs from fuzzy system
+with open(TEST_DATA_OUTPUT, 'w') as f:
+    f.write("MemoryUsage,ProcessorLoad,InpNetThroughput,OutNetThroughput,OutBandwidth,Latency,V_MemoryUsage,V_ProcessorLoad,V_InpNetThroughput,V_OutNetThroughput,V_OutBandwidth,V_Latency,CLPVariation")
+    for index, row in main.read_input(TEST_DATA_INPUT).iterrows():
+        clp = main.find_clp(row['MemoryUsage'], row['ProcessorLoad'], row['InpNetThroughput'], row['OutNetThroughput'], row['OutBandwidth'], row['Latency'], row['V_MemoryUsage'], row['V_ProcessorLoad'], row['V_InpNetThroughput'], row['V_OutNetThroughput'], row['V_OutBandwidth'], row['V_Latency'])
+        f.write(f"\n{row['MemoryUsage']},{row['ProcessorLoad']},{row['InpNetThroughput']},{row['OutNetThroughput']},{row['OutBandwidth']},{row['Latency']},{row['V_MemoryUsage']},{row['V_ProcessorLoad']},{row['V_InpNetThroughput']},{row['V_OutNetThroughput']},{row['V_OutBandwidth']},{row['V_Latency']},{clp}")
+print("Train outputs (fuzzy system) generated")
+
+print("Spliting the test dataset into validation and test")
+# split the test dataset in Validation and Test
+df_test = pd.read_csv(TEST_DATA_OUTPUT)
+data = df_test[['MemoryUsage', 'ProcessorLoad', 'OutNetThroughput', 'OutBandwidth']]
+target = df_test['CLPVariation']
+
+data_test, data_validate, target_test, target_validate = train_test_split(data, target, test_size=0.5, random_state=42)
+data_test.to_csv('data/data_test.csv', index=False)
+data_validate.to_csv('data/target_validate.csv', index=False)
+target_test.to_csv('data/target_test.csv', index=False)
+target_validate.to_csv('data/target_validate.csv', index=False)
+
+print("Test dataset into validation and test datasets splitted")
