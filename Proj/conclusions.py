e545b355dea430dd08adb127b6073e345fbba69c
samuelbarata <samuel@barata.pt>
2024-05-20 20:09:29 +0100

    regression graph

---
+++ Proj/conclusions.py
@@ -0,0 +1,21 @@
+import pandas as df
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the data
+data = df.read_csv('TestResult-FFNN.csv')
+
+x, y = data['CLPVariation'], data['NN_Regression']
+
+# scatter
+# SMALL scatter
+plt.scatter(x, y, color='red', s=0.1)
+plt.title('CLPVariation vs NN_Regression')
+plt.xlabel('CLPVariation')
+plt.ylabel('NN_Regression')
+
+# linear regression
+m, b = np.polyfit(x, y, 1)
+plt.plot(x, m*x + b)
+plt.show()
+
