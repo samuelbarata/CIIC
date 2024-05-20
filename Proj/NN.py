4f699f1b25c6f3773edef9f44b32e51ccb909658
samuelbarata <samuelbarata@delilah.barata.pt>
2024-05-20 13:56:08 +0100

    a

--- Proj/NN.py
+++ Proj/NN.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import sklearn as sl
-import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.utils.multiclass import type_of_target
 import logging
 
 """
@@ -13,27 +14,41 @@ import logging
 - Test set: A set of examples used only to assess the performance of a fully-specified classifier.
 """
 
+def transform_target(y):
+    """Transform the continuous output into discrete classes."""
+    bins = [-1, -0.2, 0.2, 1]
+    labels = ['Decrease', 'Maintain', 'Increase']
+    y_class = pd.cut(y, bins=bins, labels=labels)
+    return y_class
 
-def train(df: pd.DataFrame, hidden_layer_sizes=(1,)):
-    x = df[['MemoryUsage', 'ProcessorLoad', 'OutNetThroughput', 'OutBandwidth']]
-    y = df['CLPVariation']
-
+def train(data, target, hidden_layer_sizes=(1,)):
     mlp = MLPRegressor(
         hidden_layer_sizes=hidden_layer_sizes,
-        activation='logistic',
-        solver='sgd',
+        activation='tanh',
+        solver='adam',
         max_iter=1000,
         random_state=42
     )
-    mlp.fit(x, y)
+    mlp.fit(data, target)
     return mlp
 
-def test(mlp: MLPRegressor, df: pd.DataFrame):
-    x = df[['MemoryUsage', 'ProcessorLoad', 'OutNetThroughput', 'OutBandwidth']]
-    y = df['CLPVariation']
+def test(mlp: MLPRegressor, data, target):
+    # target = target.to_list()
 
-    y_pred = mlp.predict(x)
-    mse = mean_squared_error(y, y_pred)
+    # print(type_of_target(target))
+    y_pred = mlp.predict(data)
+    mse = mean_squared_error(target, y_pred)
+    logging.info(f"MSE: {mse}")
+    # print(type(y_pred), type(target))
+    # target = [0 for _ in range(50000000)]
+    # target = [1,2,3,4]
+    # y_pred = [1,2,3,4]
+
+    # print(accuracy_score(target, y_pred))
+    # accuracy = accuracy_score(target, y_pred)
+    # logging.info(f"ACCURACY: {accuracy}")
+    # logging.info(f"Classification Report for Test Set:\n{classification_report(target, y_pred)}")
+    # logging.info(f"Confusion Matrix for Test Set:\n{confusion_matrix(target, y_pred)}")
     return mse
 
 if __name__ == '__main__':
@@ -41,8 +56,6 @@ if __name__ == '__main__':
     # LOG_LEVEL = logging.DEBUG
     # values linearly distributed
     LEARN_DATA = 'data/TestResult-FSS_10_rng.csv'
-    # values linearly distributed that don't intersect with the learn data
-    VALIDATE_DATA = 'data/TestResult-FSS_33_rng.csv'
     # truly random values
     TEST_DATA = 'data/TestResult-FSS_rng.csv'
     FIND_BEST_MATCH = False
@@ -50,29 +63,46 @@ if __name__ == '__main__':
     logging.basicConfig(level=LOG_LEVEL, format='%(levelname)s: %(message)s')
 
     df_learn = pd.read_csv(LEARN_DATA)
-    df_validate = pd.read_csv(VALIDATE_DATA)
+    learn_data = df_learn[['MemoryUsage', 'ProcessorLoad', 'OutNetThroughput', 'OutBandwidth']]
+    learn_target = df_learn['CLPVariation']
+
     df_test = pd.read_csv(TEST_DATA)
+    data = df_test[['MemoryUsage', 'ProcessorLoad', 'OutNetThroughput', 'OutBandwidth']]
+    target = df_test['CLPVariation']
+
+    data_test, data_validate, target_test, target_validate = train_test_split(data, target, test_size=0.5, random_state=42)
+    target_test.to_csv('/tmp/target_test.csv', index=False)
+
 
     if FIND_BEST_MATCH:
         hidden_layer_sizes = [(x,) for x in range(1, 11, 1)] + [(x, y) for x in range(1, 11, 1) for y in range(1, 11, 1)]
-        min_mse = float('inf')
-        best_neuron = None
 
-        for hidden_layer_size in hidden_layer_sizes:
-            mlp = train(df_learn, hidden_layer_size)
-            mse = test(mlp, df_validate)
-            if mse < min_mse:
-                best_neuron = hidden_layer_size
-                min_mse = mse
-            logging.debug(f"Hidden Layer Size: {hidden_layer_size}, MSE: {mse}")
-        logging.info(f"Best Hidden Layer Size: {best_neuron}, MSE: {min_mse}")
+        # Define the parameter grid
+        parameter_grid = {
+            #'hidden_layer_sizes': [(x,) for x in range(1, 21, 1)],
+            'hidden_layer_sizes': [(x,) for x in range(1, 11, 1)],
+            'activation': ['tanh'], # ['relu', 'identity', 'logistic', 'tanh']
+            'solver': ['adam'],                  # ['lbfgs', 'sgd', 'adam']
+            'alpha': [0.0001],
+            'learning_rate': ['constant'],
+            'max_iter': [1000]
+        }
 
-        mlp = train(df_learn, best_neuron)
-        mse_test = test(mlp, df_test)
-        logging.info(f"TEST MSE: {mse_test}")
+        mlp = MLPRegressor(random_state=42)
+        grid_search = GridSearchCV(mlp, parameter_grid, scoring='neg_mean_squared_error', cv=3, n_jobs=-1)
+
+
+        grid_search.fit(learn_data, learn_target)
+        logging.info(f"Best parameters found: {grid_search.best_params_}")
+
+        # Evaluate the best model found by grid search
+        best_mlp = grid_search.best_estimator_
+        target_pred = best_mlp.predict(data_validate)
+        mse = mean_squared_error(target_validate, target_pred)
+        logging.info(f"Mean Squared Error with best parameters: {mse}")
     else:
-        mlp = train(df_learn, (5,))
-        mse_validate = test(mlp, df_validate)
+        mlp = train(learn_data, learn_target, (9,))
+        mse_validate = test(mlp, data_validate, target_validate)
         logging.info(f"VALIDATE MSE: {mse_validate}")
-        mse_test = test(mlp, df_test)
+        mse_test = test(mlp, data_test, target_test)
         logging.info(f"TEST MSE: {mse_test}")
